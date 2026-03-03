423351a974931ebd493f3aac677e25d6de6cfa8d
TheUIinventor <synchron.work@gmail.com>
2026-03-03 13:52:06 +1100

    tested stuff

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold dark:text-white">Home Page</h1>
-      <p className="text-zinc-500 mt-2">Welcome to Synchron. This is your home dashboard.</p>
+      <p className="text-zinc-500 mt-2">Welcome to Synchron v2. This is your home dashboard.</p>
     </div>
   );
 };
